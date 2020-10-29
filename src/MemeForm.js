1c7dc490b2a401c55465b6224f64a0491955a771
Seth <colts18seth@gmail.com>
2020-10-28 20:18:57 -0400

    changed to img and divs instead of canvas

--- src/MemeForm.js
+++ src/MemeForm.js
@@ -4,40 +4,41 @@ import meme3 from "./imgs/meme3.jpg";
 import meme4 from "./imgs/meme4.jpg";
 import { useSelector, useDispatch } from "react-redux";
 import './MemeForm.css';
-import FinishedMeme from "./FinishedMeme";
 
 function MemeForm() {
     const memes = useSelector(state => state.finishedMemes);
     const dispatch = useDispatch();
 
     const addImage = (e) => {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-        canvas.width = 500;
-        canvas.height = 500;
-        ctx.drawImage(e.target, 0, 0, 500, 500);
+        const display = document.getElementById("display");
+        if (display.firstChild) {
+            display.removeChild(display.firstChild);
+        }
+        const img = document.createElement('img');
+        img.src = e.target.src;
+        display.append(img);
 
         dispatch({ type: "ADD-IMAGE", image: e.target.src, name: e.target.alt });
     }
 
     const addTopText = () => {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-        const topTextInput = document.getElementById("topText")
-        let topText = topTextInput.value;
-        ctx.font = '40px arial';
-        ctx.fillText(topText, 10, 50);
+        const display = document.getElementById("display");
+        const topText = document.getElementById("topText");
+        const div = document.createElement('div');
+        div.innerText = topText.value;
+        div.classList.add("topText");
+        display.append(div);
 
         dispatch({ type: "ADD-TOP-TEXT", topText: topText });
     }
 
     const addBottomText = () => {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-        const bottomTextInput = document.getElementById("bottomText")
-        let bottomText = bottomTextInput.value;
-        ctx.font = '40px arial';
-        ctx.fillText(bottomText, 10, 490);
+        const display = document.getElementById("display");
+        const bottomText = document.getElementById("bottomText");
+        const div = document.createElement('div');
+        div.innerText = bottomText.value;
+        div.classList.add("bottomText");
+        display.append(div);
 
         dispatch({ type: "ADD-BOTTOM-TEXT", bottomText: bottomText });
     }
@@ -48,65 +49,59 @@ function MemeForm() {
         console.log(memes)
     }
 
-    const drawFinishedMemes = (meme) => {
-        const finishedMemes = document.getElementById("finishedMemes");
-        const canvas = document.createElement("canvas");
-        const ctx = canvas.getContext('2d');
-        let topText = meme.topText;
-        let bottomText = meme.bottomText;
-        ctx.font = '40px arial';
-        ctx.fillText(topText, 10, 50);
-        ctx.fillText(bottomText, 10, 490);
-        finishedMemes.append(canvas);
-    }
-
     return (
-        <div className="MemeForm">
-            <div id='imageSelect' className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
-                    Select Meme Image
-                </button>
-                <div className="dropdown-menu">
-                    <button onClick={addImage} className="dropdown-item">
-                        <img id="meme1" src={meme1} alt='meme1' />
-                    </button>
-                    <button onClick={addImage} className="dropdown-item">
-                        <img src={meme2} alt='meme2' />
-                    </button>
-                    <button onClick={addImage} className="dropdown-item">
-                        <img src={meme3} alt='meme3' />
-                    </button>
-                    <button onClick={addImage} className="dropdown-item">
-                        <img src={meme4} alt='meme4' />
-                    </button>
-                </div>
+        <div className="row">
+            <div className="col">
+                <div id="display"></div>
             </div>
-            <form className="mt-3">
-                <div className="form-group">
-                    <label htmlFor="topText">
-                        Top Text:
-                    </label>
-                    <input onChange={addTopText} className="form-control" id="topText" />
+            <div className="MemeForm col">
+                <div id='imageSelect' className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown">
+                        Select Meme Image
+                    </button>
+                    <div className="dropdown-menu">
+                        <button onClick={addImage} className="dropdown-item">
+                            <img id="meme1" src={meme1} alt='meme1' />
+                        </button>
+                        <button onClick={addImage} className="dropdown-item">
+                            <img src={meme2} alt='meme2' />
+                        </button>
+                        <button onClick={addImage} className="dropdown-item">
+                            <img src={meme3} alt='meme3' />
+                        </button>
+                        <button onClick={addImage} className="dropdown-item">
+                            <img src={meme4} alt='meme4' />
+                        </button>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="bottomText">
-                        Bottom Text:
-                    </label>
-                    <input onChange={addBottomText} className="form-control" id="bottomText" />
-                </div>
-                <button onClick={saveMeme} type="submit" className="btn btn-primary">
-                    Save
+                <form className="mt-3">
+                    <div className="form-group">
+                        <label htmlFor="topText">
+                            Top Text:
+                        </label>
+                        <div className="input-group mb-3">
+                            <input className="form-control" id="topText" />
+                            <div className="input-group-append">
+                                <button onClick={addTopText} className="btn btn-primary" type="button">Add</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="bottomText">
+                            Bottom Text:
+                        </label>
+                        <div className="input-group mb-3">
+                            <input className="form-control" id="bottomText" />
+                            <div className="input-group-append">
+                                <button onClick={addBottomText} className="btn btn-primary" type="button">Add</button>
+                            </div>
+                        </div>
+                    </div>
+                    <button onClick={saveMeme} type="submit" className="btn btn-primary">
+                        Save
                 </button>
-            </form>
-            <hr />
-            <div id="display">
-                <canvas id="canvas">
-                </canvas>
-            </div>
-            <div id="finishedMemes">
-                {memes &&
-                    memes.map(meme => <FinishedMeme drawFinishedMemes={drawFinishedMemes} meme={meme} />)
-                }
+                </form>
+
             </div>
         </div>
     );
